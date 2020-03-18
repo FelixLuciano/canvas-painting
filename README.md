e80ea8460b899a582e3a7ee966fee298cac90afe
Luciano Felix Dias <luciano.felix.555@hotmail.com>
2020-03-18 01:25:44 -0300

    Hello World!

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# canvas-painting
-Reusable &lt;canvas> tool with web-components
+![canvas-painting](public/banner.svg)
+
+### INTRODUCTION
+
+**canvas-painting** is a base web component with a set of methods available that go straight on the point on creating reusable canvas illustrations.
+
+
+### FEATURES
+
+**Reusable** - Once your painting is programmed, a simple html attribute can bring thousands of canvas to life, replicating the exact behavior for each one, but with datasets, it is still possible to make individual scope changes.
+
+**Straight to the point** - With the available methods, the only job is to bring the painting to the canvas.
+
+**Stats** - Monitor real-time information of his painting with [Stats](http://mrdoob.github.io/stats.js). By giving the command, the service is automatically embedded in your project.
+
+
+### GETTING STARTED
+
+TODO
+
+
+### EXAMPLES
+
+Here are some animated illustrations made on Codepen that show canvas-painting working.
+
+- [Canvas Painting Example](https://codepen.io/FelixLuciano/pen/KKpMexR) - Canvas Painting Example
+- [Flame 🔥](https://codepen.io/FelixLuciano/pen/ZEGpqdM) - Canvas generative Flame made with Trigonometry functions
+- [Cyclic Lines 🌀](https://codepen.io/FelixLuciano/pen/eYNWoGb) - Animated canvas illustration
+- [Stars ✨](https://codepen.io/FelixLuciano/pen/abOywWw) - Blinking stars
+- [Atomus 💫](https://codepen.io/FelixLuciano/pen/PoqjZpb) - Animated atomic orbitals diagram
+- [São Paulo 🏙](https://codepen.io/FelixLuciano/pen/qBdZErJ) - São Paulo streets pattern made with sine wave
+
+
+### LICENSE
+[MIT](https://github.com/FelixLuciano/canvas-painting/blob/master/LICENSE)
+
+
+**Copyright (c) 2019 Luciano Felix**
---
+++ src/index.js
@@ -0,0 +1,125 @@
+class CanvasPainting extends HTMLCanvasElement {
+  constructor () {
+    super()
+    this.$setup()
+    if (this.beforeInit) this.beforeInit()
+    if (this.$options.autoInit) {
+      this.$init()
+      if (this.afterInit) this.afterInit()
+    }
+  }
+  
+  $setup () {
+    this.ctx2D = this.getContext('2d')
+    this.$frameRequest = null
+    
+    this.$options = {
+      width: parseInt(getComputedStyle(this.parentElement).width),
+      height: parseInt(getComputedStyle(this.parentElement).height),
+      autoInit: true,
+      clear: true,
+      showStats: false
+    }
+    
+    this.$scope = {
+      background: 'transparent'
+    }
+    
+    if (this.options)
+      this.$options = Object.assign({}, this.$options, this.options())
+    
+    if(!this.$options.clear)
+      this.$scope.background = false
+    
+    this.$updateSize(this.$options.width, this.$options.height)
+    window.addEventListener('resize', () => {
+      this.$updateSize()
+      this.$clear()
+      if (this.draw) this.draw()
+    })
+    
+    if (this.scope)
+      this.$scope = Object.assign({}, this.$scope, this.scope())
+    
+    if (this.dataset)
+      this.$scope = Object.assign({}, this.$scope, this.dataset)
+      
+    if(this.$options.showStats) {
+      const src = 'https://mrdoob.github.io/stats.js/build/stats.min.js'
+      const statsScript = document.querySelector(`script[src="${src}"]`)
+      
+      const setupStats = () => {
+        this.$stats = new Stats()
+        this.$stats.dom.style.position = 'absolute'
+        this.$stats.dom.style.left = 'initial'
+        this.$stats.dom.style.top = 'initial'
+        this.parentElement.insertBefore(this.$stats.dom, this)
+      }
+      
+      if (statsScript) setupStats()
+      else {
+        const script = document.createElement('script')
+        script.src = src
+        script.onload = setupStats
+        document.head.appendChild(script)
+      }
+    }
+  }
+  
+  $init () {
+    this.$clear()
+    this.draw()
+    
+    if (this.loop) this.$loop()
+  }
+  
+  $loop () {
+    this.$clear()
+    this.loop()
+    this.draw()
+
+    if (this.$options.showStats && this.$stats)
+      this.$stats.update()
+
+    this.$frame(this.$loop)
+  }
+  
+  $frame (callback) {
+    const requestFrame = window.requestAnimationFrame ||
+                         window.webkitRequestAnimationFrame ||
+                         window.mozRequestAnimationFrame ||
+                         window.msRequestAnimationFrame ||
+                         function (cb) { setTimeout(cb, 50/3) }
+    
+    return this.$frameRequest = requestFrame(() => callback.call(this))
+  }
+  
+  $stop () {
+    const cancelFrame = window.cancelAnimationFrame ||
+                        window.mozCancelAnimationFrame ||
+                        clearTimeout
+    
+    cancelFrame(this.$frameRequest)
+    this.$frameRequest = null
+  }
+  
+  $updateSize (width, height) {
+    this.width = width || parseInt(getComputedStyle(this.parentElement).width)
+    this.height = height || parseInt(getComputedStyle(this.parentElement).height)
+  }
+  
+  $clear (color = this.$scope.background) {
+    if (color === undefined)
+      return
+    else if (color === 'transparent')
+      return this.ctx2D.clearRect(0, 0, this.width, this.height)
+    else {
+      this.ctx2D.save()
+      this.ctx2D.fillStyle = color
+      this.ctx2D.fillRect(0, 0, this.width, this.height)
+      this.ctx2D.restore()
+    }
+  }
+}
+
+module.exports = CanvasPainting
